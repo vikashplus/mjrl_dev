5e24e4b23733d17035d4ba3438324ccfed0a2d36
Vikash Kumar <vikashplus@gmail.com>
2022-02-27 00:12:43 -0500

    Update: Updating to latest kitchen and myo env names

--- mjrl_dev/hydra/train_RS_suits.sh
+++ mjrl_dev/hydra/train_RS_suits.sh
@@ -1,7 +1,7 @@
 help="
 USAGE:
 ./hydra/train_rs_tasks.sh <task> <target>
-    <task> choices:     kitchen, biomechanics
+    <task> choices:     kitchen, myo
     <target> choices:   '', local, slurm
 
 Example:
@@ -19,14 +19,14 @@ fi
 
 # Configure envs
 if [ "$1" == "kitchen" ] ; then
-    envs="kitchen_micro_open-v3,kitchen_rdoor_open-v3,kitchen_ldoor_open-v3,kitchen_sdoor_open-v3,kitchen_light_on-v3,kitchen_knob4_on-v3,kitchen_knob3_on-v3,kitchen_knob2_on-v3,kitchen_knob1_on-v3"
+    envs="kitchen_micro_open-v3,kitchen_micro_close-v3,kitchen_rdoor_open-v3,kitchen_rdoor_close-v3,kitchen_ldoor_open-v3,kitchen_ldoor_close-v3,kitchen_sdoor_open-v3,kitchen_sdoor_close-v3,kitchen_light_on-v3,kitchen_light_off-v3,kitchen_knob4_on-v3,kitchen_knob4_off-v3,kitchen_knob3_on-v3,kitchen_knob3_off-v3,kitchen_knob2_on-v3,kitchen_knob2_off-v3,kitchen_knob1_on-v3,kitchen_knob1_off-v3,franka_micro_open-v3,franka_micro_close-v3,franka_micro_random-v3,franka_slide_open-v3,franka_slide_close-v3,franka_slide_random-v3"
 
     config="--config-name hydra_kitchen_config.yaml $config"
 
-elif [ "$1" == "biomechanics" ] ; then
-    envs="FingerReachMotorFixed-v0,FingerReachMotorRandom-v0,FingerReachMuscleFixed-v0,FingerReachMuscleRandom-v0,FingerPoseMotorFixed-v0,FingerPoseMotorRandom-v0,FingerPoseMuscleFixed-v0,FingerPoseMuscleRandom-v0,ElbowPose1D1MRandom-v0,ElbowPose1D6MRandom-v0,ElbowPose1D6MExoRandom-v0,HandPoseMuscleFixed-v0,HandPoseMuscleRandom-v0,HandReachMuscleFixed-v0,HandReachMuscleRandom-v0,HandKeyTurnFixed-v0,HandKeyTurnRandom-v0,HandObjHoldFixed-v0,HandObjHoldRandom,-v0HandPenTwirlFixed-v0,HandPenTwirlRandom-v0,BaodingFixed-v1,BaodingFixed4th-v1,BaodingFixed8th-v1"
+elif [ "$1" == "myo" ] ; then
+    envs="motorFingerReachFixed-v0, motorFingerReachRandom-v0, myoFingerReachFixed-v0, myoFingerReachRandom-v0, myoHandReachFixed-v0, myoHandReachRandom-v0, motorFingerPoseFixed-v0, motorFingerPoseRandom-v0, myoFingerPoseFixed-v0, myoFingerPoseRandom-v0, myoElbowPose1D6MFixed-v0, myoElbowPose1D6MRandom-v0, myoElbowPose1D6MExoRandom-v0, myoElbowPose1D6MExoRandom-v0, myoHandPoseFixed-v0, myoHandPoseRandom-v0, myoHandKeyTurnFixed-v0, myoHandKeyTurnRandom-v0, myoHandObjHoldFixed-v0, myoHandObjHoldRandom-v0, myoHandPenTwirlFixed-v0, myoHandPenTwirlRandom-v0, myoHandBaodingFixed-v1, myoHandBaodingRandom-v1, myoHandBaodingFixed4th-v1, myoHandBaodingFixed8th-v1"
 
-    config="--config-name hydra_biomechanics_config.yaml $config"
+    config="--config-name hydra_myo_config.yaml $config"
 
 elif [ "$1" == "hand" ] ; then
     # envs="door-v0,hammer-v0,pen-v0,relocate-v0,"
