3bbc70cab4024441c8a8b68e50c217ec420fda3d
Vikash Kumar <vikashplus@gmail.com>
2023-05-17 01:59:08 -0400

    Adding rp links

--- mjrl_dev/utils/sync.sh
+++ mjrl_dev/utils/sync.sh
@@ -1,7 +1,12 @@
 echo ==== Syncing $1:$3 to $2:$3 ====
 
+
+# local => rpxx  ::  sync local remote <code_path>
+if [ "$1" == "local" ] && ( [ "$2" == "rp00" ] || [ "$2" == "rp01" ] ) ; then
+	rsync -av --progress --exclude='.git/' --exclude='*.pickle' --exclude='*.pkl' --exclude="__pycache__" ~/Libraries/$3/ $2:~/Libraries/$3/
+
 # local => UW CSE  ::  sync local remote <code_path>
-if [ "$1" == "local" ] && ( [ "$2" == "bundle" ] || [ "$2" == "voltron" ] || [ "$2" == "kakade-gpu2" ] ) ; then
+elif [ "$1" == "local" ] && ( [ "$2" == "bundle" ] || [ "$2" == "voltron" ] || [ "$2" == "kakade-gpu2" ] ) ; then
 	sshpass -p handrobot1! rsync -av --progress --exclude='.git/' --exclude='*.pickle' --exclude='*.pkl' --exclude="__pycache__" ~/Libraries/$3/ vikash@$2.cs.washington.edu:~/Libraries/$3/
 
 # local => GCP  ::  sync local gcp <code_path> <ec2_ip>
@@ -19,6 +24,11 @@ elif [ "$1" == "local" ] && [ "$2" == "devfair" ] ; then
 # Berkeley => local  :: sync remote local <log_path>
 elif [ "$1" == "newton1" ] || [ "$1" == "newton2" ] || [ "$1" == "newton3" ] || [ "$1" == "newton4" ] || [ "$1" == "newton5" ] || [ "$1" == "newton6" ] || [ "$1" == "newton7" ] || [ "$1" == "allegrobase" ] ; then
 	rsync -av --include='best_policy.pickle' --exclude='*.pickle' vikash@$1.banatao.berkeley.edu:~/Projects/$3 ~/Projects/
+
+# UW => local
+elif ( [ "$1" == "rp00" ]  || [ "$1" == "rp01" ] ) && [ "$2" == "local" ] ; then
+	rsync -av --include='best_policy.pickle' --exclude='*.pickle' --exclude='*.out' $1:~/Projects/$3 ~/Projects/
+
 
 # UW => local
 elif ( [ "$1" == "bundle" ]  || [ "$1" == "voltron" ] ) && [ "$2" == "local" ] ; then
